f86ec299f2706599ee4bee24a657d51165887f1d
jtiagosantos <tiagosan040@gmail.com>
2023-02-27 18:14:46 -0300

    chore(config): configure application to use it as a lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import * as packageJSON from './package.json';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      include: ['src/components/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'PopixUI',
+      formats: ['es', 'umd'],
+      fileName: 'popix-ui',
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJSON.devDependencies)],
+    },
+  },
+});
