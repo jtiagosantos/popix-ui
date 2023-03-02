b10396e858def947e79a6bed08c7619834b6d134
jtiagosantos <tiagosan040@gmail.com>
2023-03-01 23:54:49 -0300

    feat(components): implement button component

---
+++ src/components/button.component.tsx
@@ -0,0 +1,157 @@
+import { Spinner } from './spinner.component';
+
+import type { FC, ComponentProps } from 'react';
+import type { CSSProperties } from '@stitches/react';
+
+import { styled } from '../styles';
+
+const ButtonUI = styled('button', {
+  all: 'unset',
+  minWidth: '100px',
+  minHeight: '40px',
+  borderRadius: '6px',
+  padding: '0 16px',
+  borderWidth: '2px',
+  borderStyle: 'solid',
+  borderColor: 'transparent',
+  position: 'relative',
+
+  textAlign: 'center',
+  fontWeight: '500',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '8px',
+
+  cursor: 'pointer',
+
+  transition: 'all 0.2s ease',
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+    filter: 'brightness(0.6)',
+  },
+
+  '&:not(:disabled):hover': {
+    filter: 'brightness(0.8)',
+  },
+
+  variants: {
+    variant: {
+      solid: {
+        backgroundColor: '#6d28d9',
+        color: '#fff',
+
+        '&:not(:disabled):hover': {
+          filter: 'brightness(0.9)',
+        },
+
+        '&:disabled': {
+          filter: 'brightness(0.7)',
+        },
+      },
+
+      outline: {
+        backgroundColor: 'transparent',
+        color: '#6d28d9',
+        borderColor: '#6d28d9',
+      },
+
+      ghost: {
+        backgroundColor: 'transparent',
+        color: '#6d28d9',
+      },
+
+      link: {
+        backgroundColor: 'transparent',
+        color: '#6d28d9',
+        textDecoration: 'underline',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'solid',
+  },
+});
+
+const CustomChildrenUI = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '8px',
+
+  opacity: 0,
+});
+
+type ButtonProps = Omit<ComponentProps<typeof ButtonUI>, 'disabled'> & {
+  href?: string;
+  colorTheme?: string;
+  loadingText?: string;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+};
+
+export const ButtonHandlerProps: FC<ButtonProps> = ({
+  variant = 'solid',
+  isLoading = false,
+  isDisabled = false,
+  colorTheme,
+  children,
+  css,
+  ...props
+}) => {
+  const solid: CSSProperties = {
+    backgroundColor: colorTheme,
+  };
+  const outline: CSSProperties = {
+    color: colorTheme,
+    borderColor: colorTheme,
+  };
+  const ghost: CSSProperties = {
+    color: colorTheme,
+  };
+  const link: CSSProperties = {
+    color: colorTheme,
+  };
+  const customColors = {
+    solid,
+    outline,
+    ghost,
+    link,
+  }[variant as 'solid' | 'outline' | 'ghost' | 'link'];
+  const spinnerColor = variant === 'solid' ? '#fff' : colorTheme;
+
+  return (
+    <ButtonUI
+      variant={variant}
+      disabled={isDisabled || isLoading}
+      css={{ ...customColors, ...css }}
+      {...props}>
+      {!isLoading && <>{children}</>}
+
+      {isLoading && (
+        <>
+          <Spinner color={spinnerColor} css={{ position: 'absolute' }} />
+          <CustomChildrenUI>{children}</CustomChildrenUI>
+        </>
+      )}
+    </ButtonUI>
+  );
+};
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { variant, href } = props;
+  const buttonIsLink = variant === 'link';
+
+  if (buttonIsLink) {
+    return (
+      <a href={href}>
+        <ButtonHandlerProps {...props} />
+      </a>
+    );
+  }
+
+  return <ButtonHandlerProps {...props} />;
+};
