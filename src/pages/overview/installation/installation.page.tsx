f153e797e27afe5fd500fb0d7d1514dad9fd4823
jtiagosantos <tiagosan040@gmail.com>
2023-03-06 12:17:24 -0300

    feat(pages): implement installation page

---
+++ src/pages/overview/installation/installation.page.tsx
@@ -0,0 +1,34 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+import npmIcon from '../../../assets/npm-icon.svg';
+import yarnIcon from '../../../assets/yarn-icon.svg';
+
+import { Container, Installation, PackageManager } from './installation.styles';
+
+export const InstallationPage = () => {
+  const installationWithNPM = `npm i popix-ui`;
+  const installationWithYarn = `yarn add popix-ui`;
+
+  return (
+    <Container>
+      <p>Para usar Popix UI em seu projeto, execute um dos comandos no seu terminal:</p>
+
+      <Installation>
+        <PackageManager variant="npm">
+          <img src={npmIcon} alt="npm" />
+          <p>npm</p>
+        </PackageManager>
+        <SyntaxHighlighter style={atelierCaveDark}>{installationWithNPM}</SyntaxHighlighter>
+      </Installation>
+
+      <Installation>
+        <PackageManager variant="yarn">
+          <img src={yarnIcon} alt="yarn" />
+          <p>yarn</p>
+        </PackageManager>
+        <SyntaxHighlighter style={atelierCaveDark}>{installationWithYarn}</SyntaxHighlighter>
+      </Installation>
+    </Container>
+  );
+};
---
+++ src/pages/overview/installation/installation.styles.ts
@@ -0,0 +1,46 @@
+import { styled } from '../../../styles';
+
+export const Container = styled('div', {
+  maxW: '720px',
+  w: '100%',
+  p: '32px 24px',
+});
+
+export const Installation = styled('div', {
+  w: '100%',
+  mt: '24px',
+
+  [`& > pre`]: {
+    p: '16px !important',
+    br: '6px',
+    bgColor: '#282c34 !important',
+    fontSize: '16px',
+  },
+});
+
+export const PackageManager = styled('span', {
+  mb: '3px',
+
+  display: 'flex',
+  alignItems: 'center',
+  gap: '6px',
+
+  [`& > p`]: {
+    fontWeight: '500',
+  },
+
+  variants: {
+    variant: {
+      npm: {
+        [`& > p`]: {
+          color: '#E53E3E',
+        },
+      },
+      yarn: {
+        [`& > p`]: {
+          color: '#3182ce',
+        },
+      },
+    },
+  },
+});
