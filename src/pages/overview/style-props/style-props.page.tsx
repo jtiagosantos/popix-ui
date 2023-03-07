a50247f6e5c96b6c2cdb5f02efab7e9d9eb9030d
jtiagosantos <tiagosan040@gmail.com>
2023-03-07 18:31:07 -0300

    feat(style-props page): handle SEO

--- src/pages/overview/style-props/style-props.page.tsx
+++ src/pages/overview/style-props/style-props.page.tsx
@@ -1,28 +1,38 @@
+import { Helmet } from 'react-helmet';
+
 import { tableData } from './style-props-table-data';
 
 import * as S from './style-props.styles';
 
 export const StylePropsPage = () => {
   return (
-    <S.Container>
-      <p>A tabela a seguir mostra cada alias e seu respectivo estilo em CSS:</p>
+    <>
+      <Helmet>
+        <title>Style Props - Popix UI</title>
+        <meta name="title" content="Style Props - Popix UI" />
+        <meta name="description" content="PopixUI style props guide" />
+      </Helmet>
 
-      <S.Table>
-        <thead>
-          <tr>
-            <th>Alias</th>
-            <th>Propriedade CSS</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map(({ alias, css }, index) => (
-            <tr key={index}>
-              <td>{alias}</td>
-              <td>{css}</td>
+      <S.Container>
+        <p>A tabela a seguir mostra cada alias e seu respectivo estilo em CSS:</p>
+
+        <S.Table>
+          <thead>
+            <tr>
+              <th>Alias</th>
+              <th>Propriedade CSS</th>
             </tr>
-          ))}
-        </tbody>
-      </S.Table>
-    </S.Container>
+          </thead>
+          <tbody>
+            {tableData.map(({ alias, css }, index) => (
+              <tr key={index}>
+                <td>{alias}</td>
+                <td>{css}</td>
+              </tr>
+            ))}
+          </tbody>
+        </S.Table>
+      </S.Container>
+    </>
   );
 };
