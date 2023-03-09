12bca70296169a0c06d13d1f705338f26403b04e
jtiagosantos <tiagosan040@gmail.com>
2023-03-09 09:35:43 -0300

    feat(spinner page): handle SEO

--- src/docs/pages/components/spinner/spinner.page.tsx
+++ src/docs/pages/components/spinner/spinner.page.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { CodeBlock, PageContainer, PageSection, HighlightedText } from '../../../components';
 import { Spinner } from '../../../../library';
 
@@ -7,59 +8,67 @@ import { SpinnerGroup } from './spinner.styles';
 
 export const SpinnerPage = () => {
   return (
-    <PageContainer>
-      <PageSection>
-        <h2>Importar</h2>
-        <CodeBlock css={{ mt: '6px' }}>{codes.import}</CodeBlock>
-      </PageSection>
+    <>
+      <Helmet>
+        <title>Spinner - Popix UI</title>
+        <meta name="title" content="Spinner - Popix UI" />
+        <meta name="description" content="PopixUI spinner component guide" />
+      </Helmet>
 
-      <PageSection>
-        <h2>Tamanhos</h2>
-        <p>
-          Por padrão, o componente tem largura e altura de <HighlightedText>20px</HighlightedText>,
-          mas é possível alterar passando um valor personalizado para a propriedade{' '}
-          <HighlightedText>size</HighlightedText>.
-        </p>
-        <SpinnerGroup css={{ mt: '16px' }}>
-          <Spinner />
-          <Spinner size={30} />
-          <Spinner size={40} />
-          <Spinner size={50} />
-          <Spinner size={60} />
-        </SpinnerGroup>
-        <CodeBlock css={{ mt: '12px' }}>{codes.sizes}</CodeBlock>
-      </PageSection>
+      <PageContainer>
+        <PageSection>
+          <h2>Importar</h2>
+          <CodeBlock css={{ mt: '6px' }}>{codes.import}</CodeBlock>
+        </PageSection>
 
-      <PageSection>
-        <h2>Cores</h2>
-        <p>
-          Por padrão, a cor do spinner é <HighlightedText>#6d28d9</HighlightedText>, mas é possível
-          alterar usando a propriedade <HighlightedText>color</HighlightedText>.
-        </p>
-        <SpinnerGroup css={{ mt: '16px' }}>
-          <Spinner color="#16a34a" size={40} />
-          <Spinner color="#be185d" size={40} />
-          <Spinner color="#ea580c" size={40} />
-          <Spinner color="#0284c7" size={40} />
-        </SpinnerGroup>
-        <CodeBlock css={{ mt: '12px' }}>{codes.colors}</CodeBlock>
-      </PageSection>
+        <PageSection>
+          <h2>Tamanhos</h2>
+          <p>
+            Por padrão, o componente tem largura e altura de <HighlightedText>20px</HighlightedText>
+            , mas é possível alterar passando um valor personalizado para a propriedade{' '}
+            <HighlightedText>size</HighlightedText>.
+          </p>
+          <SpinnerGroup css={{ mt: '16px' }}>
+            <Spinner />
+            <Spinner size={30} />
+            <Spinner size={40} />
+            <Spinner size={50} />
+            <Spinner size={60} />
+          </SpinnerGroup>
+          <CodeBlock css={{ mt: '12px' }}>{codes.sizes}</CodeBlock>
+        </PageSection>
 
-      <PageSection>
-        <h2>Espessura</h2>
-        <p>
-          Por padrão, a espessura do spinner é de <HighlightedText>2px</HighlightedText>, mas é
-          possível alterar usando a propriedade <HighlightedText>thickness</HighlightedText>.
-        </p>
-        <SpinnerGroup css={{ mt: '16px' }}>
-          <Spinner size={40} />
-          <Spinner size={40} thickness={3} />
-          <Spinner size={40} thickness={4} />
-          <Spinner size={40} thickness={5} />
-          <Spinner size={40} thickness={6} />
-        </SpinnerGroup>
-        <CodeBlock css={{ mt: '12px' }}>{codes.thickness}</CodeBlock>
-      </PageSection>
-    </PageContainer>
+        <PageSection>
+          <h2>Cores</h2>
+          <p>
+            Por padrão, a cor do spinner é <HighlightedText>#6d28d9</HighlightedText>, mas é
+            possível alterar usando a propriedade <HighlightedText>color</HighlightedText>.
+          </p>
+          <SpinnerGroup css={{ mt: '16px' }}>
+            <Spinner color="#16a34a" size={40} />
+            <Spinner color="#be185d" size={40} />
+            <Spinner color="#ea580c" size={40} />
+            <Spinner color="#0284c7" size={40} />
+          </SpinnerGroup>
+          <CodeBlock css={{ mt: '12px' }}>{codes.colors}</CodeBlock>
+        </PageSection>
+
+        <PageSection>
+          <h2>Espessura</h2>
+          <p>
+            Por padrão, a espessura do spinner é de <HighlightedText>2px</HighlightedText>, mas é
+            possível alterar usando a propriedade <HighlightedText>thickness</HighlightedText>.
+          </p>
+          <SpinnerGroup css={{ mt: '16px' }}>
+            <Spinner size={40} />
+            <Spinner size={40} thickness={3} />
+            <Spinner size={40} thickness={4} />
+            <Spinner size={40} thickness={5} />
+            <Spinner size={40} thickness={6} />
+          </SpinnerGroup>
+          <CodeBlock css={{ mt: '12px' }}>{codes.thickness}</CodeBlock>
+        </PageSection>
+      </PageContainer>
+    </>
   );
 };
