4e535345ffc59f05e0546932b256e30d81311179
jtiagosantos <tiagosan040@gmail.com>
2023-03-25 14:14:01 -0300

    feat(docs): implement page about stack component

---
+++ src/docs/pages/components/stack/stack.styles.ts
@@ -0,0 +1,12 @@
+import { styled } from '../../../../library';
+
+export const Box = styled('div', {
+  square: '40px',
+  bgColor: '#6d28d9',
+  br: '5px',
+  color: '#fafafa',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
