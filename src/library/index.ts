4fa6d1befc1ab4cab6dcde56967d25b23b5e470b
jtiagosantos <tiagosan040@gmail.com>
2023-04-09 14:22:51 -0300

    feat(library): implement box component

--- src/library/index.ts
+++ src/library/index.ts
@@ -3,3 +3,4 @@ export { styled, globalCSS, keyframes, getCssText } from './styles';
 export * from './components/button.component';
 export * from './components/spinner.component';
 export * from './components/stack.component';
+export * from './components/box.component';
