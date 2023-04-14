d2acddeab4cba95af980704f76962319b437d01e
jtiagosantos <tiagosan040@gmail.com>
2023-04-14 15:53:24 -0300

    feat(docs): implement page about heading component

---
+++ src/docs/pages/components/heading/heading.codes.ts
@@ -0,0 +1,11 @@
+export const codes = {
+  import: `import { Heading } from 'popix-ui';`,
+  examples: `<Stack spacing="24px">
+  <Heading>Popix-ui</Heading>
+  <Heading as="h2">Popix-ui</Heading>
+  <Heading as="h3">Popix-ui</Heading>
+  <Heading as="h4">Popix-ui</Heading>
+  <Heading as="h5">Popix-ui</Heading>
+  <Heading as="h6">Popix-ui</Heading>
+</Stack>`,
+};
