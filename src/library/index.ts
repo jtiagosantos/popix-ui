c42a6a2de8b02931ace48b935081a652bab959a2
jtiagosantos <tiagosan040@gmail.com>
2023-04-14 15:52:48 -0300

    feat(library): implement Heading component

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,6 +1,7 @@
 export { styled, globalCSS, keyframes, getCssText } from './styles';
 
+export * from './components/box.component';
 export * from './components/button.component';
+export * from './components/heading.component';
 export * from './components/spinner.component';
 export * from './components/stack.component';
-export * from './components/box.component';
