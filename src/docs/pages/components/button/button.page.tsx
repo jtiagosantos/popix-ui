90d0071f94a47bef649eb2856be8441aab999122
jtiagosantos <tiagosan040@gmail.com>
2023-04-30 14:55:59 -0300

    refactor(docs): refactor page about button component

--- src/docs/pages/components/button/button.page.tsx
+++ src/docs/pages/components/button/button.page.tsx
@@ -1,8 +1,8 @@
 import { Helmet } from 'react-helmet';
 import { Envelope, Wrench, ArrowRight, ArrowLeft } from 'phosphor-react';
 
+import { Button, Text, Heading } from '../../../../library';
 import { CodeBlock, PageContainer, PageSection, HighlightedText } from '../../../components';
-import { Button } from '../../../../library';
 
 import { codes } from './button.codes';
 
@@ -19,20 +19,20 @@ export const ButtonPage = () => {
 
       <PageContainer>
         <PageSection>
-          <h2>Importar</h2>
+          <Heading as="h2">Importar</Heading>
           <CodeBlock css={{ mt: '6px' }}>{codes.import}</CodeBlock>
         </PageSection>
 
         <PageSection>
-          <h2>Variantes</h2>
-          <p>
+          <Heading as="h2">Variantes</Heading>
+          <Text>
             Use a propriedade <HighlightedText>variant</HighlightedText> para alterar o estilo do
             botão. Os valores possíveis são <HighlightedText>solid</HighlightedText>,{' '}
             <HighlightedText>oultine</HighlightedText>, <HighlightedText>ghost</HighlightedText> e{' '}
             <HighlightedText>link</HighlightedText>. Por padrão o valor é{' '}
             <HighlightedText>solid</HighlightedText>.
-          </p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          </Text>
+          <ButtonGroup>
             <Button variant="solid">Solid</Button>
             <Button variant="outline">Outline</Button>
             <Button variant="ghost">Ghost</Button>
@@ -42,12 +42,12 @@ export const ButtonPage = () => {
         </PageSection>
 
         <PageSection>
-          <h2>Cores</h2>
-          <p>
+          <Heading as="h2">Cores</Heading>
+          <Text>
             Por padrão, a cor do botão é <HighlightedText>#6d28d9</HighlightedText>, mas é possível
             alterar usando a propriedade <HighlightedText>colorTheme</HighlightedText>.
-          </p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          </Text>
+          <ButtonGroup>
             <Button variant="solid" colorTheme="#16a34a">
               Solid
             </Button>
@@ -65,13 +65,13 @@ export const ButtonPage = () => {
         </PageSection>
 
         <PageSection>
-          <h2>Estado de desabilitado</h2>
-          <p>
+          <Heading as="h2">Estado de desabilitado</Heading>
+          <Text>
             Por padrão, o botão está habilitado ao clique, mas é possível alterar esse estado
             passando o valor <HighlightedText>true</HighlightedText> para a propriedade{' '}
             <HighlightedText>isDisabled</HighlightedText>.
-          </p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          </Text>
+          <ButtonGroup>
             <Button variant="solid" isDisabled={true}>
               Solid
             </Button>
@@ -89,14 +89,14 @@ export const ButtonPage = () => {
         </PageSection>
 
         <PageSection>
-          <h2>Estado de carregamento</h2>
-          <p>
+          <Heading as="h2">Estado de carregamento</Heading>
+          <Text>
             Para ativar o estado de carregamento, basta passar o valor{' '}
             <HighlightedText>true</HighlightedText> para a propriedade{' '}
             <HighlightedText>isLoading</HighlightedText>. Com isso, o botão mostrará um spinner no
             lugar no texto e a largura se manterá inalterada.
-          </p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          </Text>
+          <ButtonGroup>
             <Button variant="solid" isLoading={true}>
               Solid
             </Button>
@@ -114,9 +114,9 @@ export const ButtonPage = () => {
         </PageSection>
 
         <PageSection>
-          <h2>Uso de ícones</h2>
-          <p>Para fazer uso de ícones, basta usar o ícone junto do texto do botão.</p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          <Heading as="h2">Uso de ícones</Heading>
+          <Text>Para fazer uso de ícones, basta usar o ícone junto do texto do botão.</Text>
+          <ButtonGroup>
             <Button variant="solid">
               <Envelope size={20} />
               Enviar e-mail
@@ -131,8 +131,8 @@ export const ButtonPage = () => {
             </Button>
           </ButtonGroup>
           <CodeBlock css={{ my: '16px' }}>{codes.icons.left}</CodeBlock>
-          <p>Ou, se preferir, o ícone do lado direito</p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          <Text>Ou, se preferir, o ícone do lado direito</Text>
+          <ButtonGroup>
             <Button variant="solid">
               Enviar e-mail
               <Envelope size={20} />
@@ -150,13 +150,13 @@ export const ButtonPage = () => {
         </PageSection>
 
         <PageSection>
-          <h2>Botão link</h2>
-          <p>
+          <Heading as="h2">Botão link</Heading>
+          <Text>
             Para o caso de uso de <HighlightedText>variant</HighlightedText> como{' '}
             <HighlightedText>link</HighlightedText>, é necessário também passar a propriedade{' '}
             <HighlightedText>href</HighlightedText> com o link desejado para navegação.
-          </p>
-          <ButtonGroup css={{ mt: '16px' }}>
+          </Text>
+          <ButtonGroup>
             <Button variant="link" href="#">
               Navegar
             </Button>
